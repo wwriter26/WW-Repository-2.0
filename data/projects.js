2ff6f256eddd999a525a26577c6973adacbea68b
William Writer <111819260+wwriter26@users.noreply.github.com>
2023-09-25 15:56:24 -0600

    Add files via upload

---
+++ data/projects.js
@@ -0,0 +1,22 @@
+const projects = [{
+    name: 'Lightning Reader',
+    image: 'images/LightningReader.png',
+    description: 'This website offers a valuable <br> solution for overcoming sub-vocalization, a common <br>habit that hinders reading speed. By effectively <br>eliminating sub-vocalization, users can experience a <br>substantial increase in their reading speed, surpassing <br>more than double their typical rate.',
+    
+    language1: 'HTML',
+    language2: 'JS',
+    language3: 'CSS',
+    background: 'images/background.jpeg',
+    website: 'https://lightningreader.org'
+  },
+  {
+    name: 'Follow Me Back',
+    image: 'images/InstagramFF.png',
+    description: 'This website allows users to find out who is <br> following them back on Instagram. Users can also <br> find out who they are not following back. It serves <br>  as a valuable instrument for those seeking to <br> unclutter their Instagram account. However, due to <br>  constraints in the API, the error rate is significantly <br> high and is only capable of accessing and <br> contrasting 100 followers and 100 followed users.',
+    language1: 'JavaScript',
+    language2: 'HTML',
+    language3: 'CSS',
+    background: 'images/background.jpeg',
+    website: 'https://followmeback.org'
+  }
+];
